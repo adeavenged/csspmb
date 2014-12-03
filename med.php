a3bb12f5d0c50d894e12f6e4a833b6f4e337bfc3
adeavenged <dheendra7393@gmai.com>
2014-12-03 15:43:31 +0700

    Commit Saya1

---
+++ med.php
@@ -0,0 +1,67 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>Penerimaan Mahasiswa Baru Online</title>
+<link rel="stylesheet" type="text/css" href="css/style.css" />
+<link rel="stylesheet" type="text/css" href="css/cssmenu.css" />
+<link rel="stylesheet" type="text/css" href="css/font-ubuntu.css" />
+
+<script type="text/javascript" src="js/jquery-1.10.2.js"></script>
+<script type="text/javascript" src="js/jquery-ui-1.10.1.custom.min.js"></script>
+<script type="text/javascript" src="js/script.js"></script>
+</head>
+
+<body>
+	<div id="wr">
+    	<div id="header"></div>
+        
+        <div id="menu">
+        	<div id="cssmenu">
+            	<ul>
+                	<li class='active'><a href='#'>Home</a></li>
+                    <li class="has-sub"><a href='#'>Program Studi</a>
+                    	<ul>
+                        	<li><a href='#'>Teknik Informatika</a></li>
+                            <li><a href='#'>Sistem Informasi</a></li>
+                            <li><a href='#'>Sistem Komputer</a></li>
+                        </ul>
+                    </li>
+                    <li><a href='#'>Informasi Gelombang</a></li>
+                    <li><a href='#'>Beasiswa</a></li>
+                    <li><a href='#'>Informasi Biaya</a></li>
+                    <li><a href='#'>Daftar</a></li>
+                </ul>
+            </div>
+        </div>
+        <!-- End Menu -->
+        <div id="listLine">
+        	<marquee direction="left" scrolldelay="20" scrollamount="2" onmouseover="this.stop()" onmouseout="this.start()"> Penerimaan Mahasiswa Baru Online STIKOM Dinamika Bangsa Jambi ...</marquee>
+        </div>
+        
+        <div id="content">
+            <div id="right">
+            	<h1>Heading 1</h1>
+            	<h2>Heading 2</h2>
+                <h3>Heading 3</h3>
+            </div>
+            
+            <div id="left">
+            	<form>
+                	<input type="text" name="usernmae" placeholder="Username ..." />
+                    <select name="tanggal">
+                    	<option value="1">Pekerjaan yang disukai</option>
+                        <option value="1">Pekerjaan yang disukai</option>
+                        <option value="1">Pekerjaan yang disukai</option>
+                        <option value="1">Pekerjaan yang disukai</option>
+                    </select>
+                </form>
+            </div>
+            
+            <div style="clear:both; width:100%"></div>
+        </div>
+        
+        <div id="footer"></div>
+    </div>
+</body>
+</html>
